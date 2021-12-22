e0393b59750ef71bac47a46d968d1480667df967
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2021-12-22 19:27:15 -0300

    ej_11.js added

---
+++ ej_11.js
@@ -0,0 +1,7 @@
+// Write a function that takes a number as argument
+// Round the number to the 2nd digit after the comma
+// Return the rounded number
+
+const roundedNumber = number => number.toFixed(2)
+
+console.log(roundedNumber(3.3333))
