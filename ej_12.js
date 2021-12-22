3ffb3200e33ba7d034873ce02defdf74055fc4c1
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2021-12-22 19:31:54 -0300

    ej_12.js

---
+++ ej_12.js
@@ -0,0 +1,10 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+const isEqual = (a,b) => {
+  return a === b ? true : false
+}
+
+console.log(isEqual(10,'10'))
+console.log("-------------")
+console.log(isEqual(10,10))
