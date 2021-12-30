e13236624a3d9045649b958acb23c177b86b46fd
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2021-12-30 14:22:03 -0300

    add ej_1.js

--- ej_1.js
+++ ej_1.js
@@ -1,2 +1,11 @@
 // Write a function that takes an object with two properties as argument
 // It should return the value of the property with key country
+
+ 
+const getCountry = obj => {
+  return obj.country
+}
+
+console.log(getCountry({  continent: 'Asia',  country: 'Japan'})) // Expected: 'Japan'
+console.log(getCountry({  country: 'Sweden',  continent: 'Europe'})) // Expected: 'Sweden'
+
