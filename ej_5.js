daac19f28f55689749424e1536f860530d59ad7c
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2022-01-05 21:45:11 -0300

    ej_5.js

--- ej_5.js
+++ ej_5.js
@@ -1,3 +1,12 @@
 // Write a function that takes a Set and a value as argument
 // If existing in the Set, remove the value from the Set
 // Return the result
+
+const removeFromSet = (set, item) => {
+  set.delete(item)
+  return set
+}
+
+console.log(removeFromSet(new Set([1, 2, 3]), 1)) // 🌟 Expected: new Set([2, 3])
+console.log(removeFromSet(new Set('12345'), '3')) // 🌟 Expected: new Set(['1', '2', '4', '5'])
+console.log(removeFromSet(new Set([1, 2, 3]), 4)) // 🌟 Expected: new Set([1, 2, 3])
